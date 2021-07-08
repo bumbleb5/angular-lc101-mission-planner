2627b09e174e1c5c354f115ff21e3558f5a8299b
bumbleb5 <69547355+bumbleb5@users.noreply.github.com>
2021-07-07 20:24:46 -0500

    working on crew component

--- src/app/crew/crew.component.ts
+++ src/app/crew/crew.component.ts
@@ -7,23 +7,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CrewComponent implements OnInit {
 
-  inCrew: boolean = false;
-  crew: object[] = [];
+    inCrew: boolean = false;
 
-  candidates: object[] = [
-    {name: "Sally Ride", photo: 'https://handlers.education.launchcode.org/static/images/sally-ride.jpg'},
-    {name: "Mae Jemison", photo: 'https://handlers.education.launchcode.org/static/images/mae-jemison.jpg'},
-    {name: "Ellen Ochoa", photo: 'https://handlers.education.launchcode.org/static/images/ellen-ochoa.jpg'},
-    {name: "Frederick Gregory", photo: 'https://handlers.education.launchcode.org/static/images/frederick-gregory.jpg'},
-    {name: "Guion Bluford", photo: 'https://handlers.education.launchcode.org/static/images/guion-bluford.jpg'},
-    {name: "Kjell Lindgren", photo: 'https://handlers.education.launchcode.org/static/images/kjell-lindgren.jpg'},
-    {name: "Jeanette Epps", photo: 'https://handlers.education.launchcode.org/static/images/jeanette-epps.jpg'}
-  ];
+    crew: object[] = [];
 
-  constructor() { }
+    candidates: object[] = [
+        {name: "Sally Ride", photo: 'https://handlers.education.launchcode.org/static/images/sally-ride.jpg'},
+        {name: "Mae Jemison", photo: 'https://handlers.education.launchcode.org/static/images/mae-jemison.jpg'},
+        {name: "Ellen Ochoa", photo: 'https://handlers.education.launchcode.org/static/images/ellen-ochoa.jpg'},
+        {name: "Frederick Gregory", photo: 'https://handlers.education.launchcode.org/static/images/frederick-gregory.jpg'},
+        {name: "Guion Bluford", photo: 'https://handlers.education.launchcode.org/static/images/guion-bluford.jpg'},
+        {name: "Kjell Lindgren", photo: 'https://handlers.education.launchcode.org/static/images/kjell-lindgren.jpg'},
+        {name: "Jeanette Epps", photo: 'https://handlers.education.launchcode.org/static/images/jeanette-epps.jpg'}
+    ];
 
-  ngOnInit() { }
+    constructor() { }
 
-  // Code the 'addCrewMember' function here:
+    ngOnInit() { }
+
+    addCrewMember(member: object): void {
+        if (!this.crew.includes(member) && this.crew.length < 3) {
+            
+            this.crew.push(member);
+        
+        } else {
+            
+            this.crew.splice(this.candidates.indexOf(member), 1);
+
+        }
+    }
 
 }
